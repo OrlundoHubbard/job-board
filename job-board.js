0700ccbbe96a368fbff88e8649bc8758fe60d667
Orlundo Hubbard <weareprojectcode@gmail.com>
2023-04-07 16:48:12 -0500

    new js

--- job-board.js
+++ job-board.js
@@ -13,4 +13,22 @@ jobForm.addEventListener('submit', (event) => {
     const title = document.querySelector('#title').value;
     const description = document.querySelector('#description').value;
     const company = document.querySelector('#company').value;
-})
+
+    // Create a new job object
+    const job = {
+        title: title,
+        description: description,
+        company: company
+    };
+
+    // Add the job to the list of jobs
+    jobs.push(job);
+
+    // Reset the form
+    jobForm.reset();
+
+    // Render the job list
+    renderJobs();
+});
+
+// Function to render the list of jobs
